33eca8d371702c2483be80307db46e7c58ee07de
kocerenes <eneskocerr41@gmail.com>
2022-05-04 14:44:09 +0300

    Create bottom bar menu.

--- app/src/main/java/com/example/appcentvideogames/MainActivity.kt
+++ app/src/main/java/com/example/appcentvideogames/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.appcentvideogames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint //hilt için
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(bottomNav,navHostFragment.navController)
     }
 }
