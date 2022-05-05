4832c76b205feeac9f36e25a0d1bfe0985d09f6e
kocerenes <eneskocerr41@gmail.com>
2022-05-05 16:23:02 +0300

    Create splash screen.

--- app/src/main/java/com/example/appcentvideogames/MainActivity.kt
+++ app/src/main/java/com/example/appcentvideogames/MainActivity.kt
@@ -10,10 +10,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint //hilt için
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme) // splash screen için
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //bottom bar ile sayfa geçişi
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         NavigationUI.setupWithNavController(bottomNav,navHostFragment.navController)
     }
